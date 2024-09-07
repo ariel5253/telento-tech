7f26c534afcc6a839003410c27deb2f66096ce97
Jesús Ariel González Bonilla <arie5253@hotmail.com>
2024-09-06 21:17:12 -0500

    Cambios - 2024-09-06

---
+++ 07-Sesion-06-09/proyecto/nativo/js/tabla.js
@@ -0,0 +1,35 @@
+function Calcular(){  
+    //Saludo
+    MsgBox();
+    
+    var html = "";
+    var numero = parseInt(document.getElementById("numero").value);
+    for(var i = 1; i<= 10; i++){
+
+        // html += "<tr>";
+        // html += "<td>1 x 2 = 5</td>";
+        // html += "</tr>";
+
+        html += `<tr>
+                    <td colspan="2">`+numero+` x `+i+` = `+(numero*i)+`</td>
+                </tr>`;
+    }
+
+    document.getElementById("tablaMultiplicar").innerHTML = html;
+    
+    Clear();
+}
+
+function MsgBox(){
+    // alert("Hola");
+
+    Swal.fire({
+        title: "Hola, probando Sweet Alert!",
+        text: "Genial!",
+        icon: "success"
+      });
+}
+
+function Clear(){
+    document.getElementById("numero").value = "";
+}
